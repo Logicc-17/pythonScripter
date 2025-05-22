d0ea75d510e50c01e6702de99cbb357157fbdf84
Yokaii <63921810+Logicc-17@users.noreply.github.com>
2025-05-22 06:58:06 -0500

    Create StrongPasswordGenerator.py

---
+++ StrongPasswordGenerator.py
@@ -0,0 +1,8 @@
+import secrets
+import string
+
+def generate_password(length=12):
+    chars = string.ascii_letters + string.digits + "!@#$%"
+    return ''.join(secrets.choice(chars) for _ in range(length))
+
+print("Your password:", generate_password())
